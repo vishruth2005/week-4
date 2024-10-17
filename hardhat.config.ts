12216e0343d991956a7c11102b94a1bb2c93293f
Apoorva Agrawal <90238207+imApoorva36@users.noreply.github.com>
2024-10-17 19:16:26 +0530

    Update hardhat.config.ts

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -14,7 +14,7 @@ import { HardhatUserConfig } from "hardhat/config";
 const config: HardhatUserConfig = {
   solidity: "0.8.0",
   networks: {
-    ropsten: {
+    sepolia: {
       url: `https://eth-sepolia.alchemyapi.io/v2/${process.env.ALCHEMY_API_KEY}`,
       accounts: [process.env.ETH_PRIVATE_KEY],
     },
