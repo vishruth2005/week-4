3f3f055157714c00e4632cba9ed4082139199b1a
Apoorva Agrawal <90238207+imApoorva36@users.noreply.github.com>
2024-10-17 13:22:49 +0000

    added necessary files

---
+++ hardhat.config.ts
@@ -0,0 +1,24 @@
+import("@nomiclabs/hardhat-ethers");
+import("@nomiclabs/hardhat-waffle");
+import dotenv from "dotenv";
+
+const argv = JSON.parse(env("npm_config_argv"));
+if (argv.original !== ["hardhat", "test"]) {
+  require('dotenv').config();
+}
+
+import("./tasks/nft");
+
+import { HardhatUserConfig } from "hardhat/config";
+
+const config: HardhatUserConfig = {
+  solidity: "0.8.0",
+  networks: {
+    ropsten: {
+      url: `https://eth-ropsten.alchemyapi.io/v2/${process.env.ALCHEMY_API_KEY}`,
+      accounts: [process.env.ETH_PRIVATE_KEY],
+    },
+  },
+};
+
+export default config;
