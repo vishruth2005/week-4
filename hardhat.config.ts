a3280090006c5931c96e67056afc848de18d1172
Apoorva Agrawal <90238207+imApoorva36@users.noreply.github.com>
2024-10-17 19:14:44 +0530

    updated to sepolia network

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -15,7 +15,7 @@ const config: HardhatUserConfig = {
   solidity: "0.8.0",
   networks: {
     ropsten: {
-      url: `https://eth-ropsten.alchemyapi.io/v2/${process.env.ALCHEMY_API_KEY}`,
+      url: `https://eth-sepolia.alchemyapi.io/v2/${process.env.ALCHEMY_API_KEY}`,
       accounts: [process.env.ETH_PRIVATE_KEY],
     },
   },
